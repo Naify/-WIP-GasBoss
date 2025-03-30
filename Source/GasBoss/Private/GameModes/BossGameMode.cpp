148387d17ace43f7bba0a9e32bc48e0b99fc3634
Naify <naify@pm.me>
2025-03-31 00:24:02 +0200

    Initial commit

---
+++ Source/GasBoss/Private/GameModes/BossGameMode.cpp
@@ -0,0 +1,5 @@
+// Naify
+
+
+#include "GameModes/BossGameMode.h"
+
---
+++ Source/GasBoss/Public/GameModes/BossGameMode.h
@@ -0,0 +1,17 @@
+// Naify
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BossGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GASBOSS_API ABossGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
